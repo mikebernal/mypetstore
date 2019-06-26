a3a31037bab14a7fe5fc996002b2e2dced03bf89
mikebernal <mikebernal.me@gmail.com>
2019-06-26 18:14:06 +0800

    Initial commit

--- web/themes/custom/omegasubtheme/js/app.js
+++ web/themes/custom/omegasubtheme/js/app.js
@@ -1,15 +1,30 @@
-var toggleMenu = document.querySelector('.duk-icon-bars');
-var menuMain   = document.querySelector('.menu--main');
+(function ($) { 
+  // "use strict";
+  Drupal.behaviors.toggleMenu = {         
+    attach: function (context, settings) {
+      $('.duk-icon-bars', context).click(function () {
 
-function toggle() {
-  if ( (menuMain.style['display'] === '') || (menuMain.style['display'] === 'none') ) {
-    menuMain.style['display'] = 'block';
-    toggleMenu.className = "duk-icon-close";
-  } else {
-    menuMain.style["display"] = "none";
-    toggleMenu.className = "duk-icon-bars";
+        var toggleMenu = $('.duk-icon-bars', context);
+        var menuMain   = $('.menu--main', context);
 
-  }
-}
+        if ( (menuMain.css('display') === '') || (menuMain.css('display') === 'none') ) {
 
-toggleMenu.addEventListener('click', toggle);
+          menuMain.css('display', 'block');
+
+          toggleMenu.removeClass('duk-icon-bars');
+          toggleMenu.addClass('duk-icon-close');
+          console.log();
+
+        } else {
+
+          menuMain.css('display', 'none');
+
+          toggleMenu.removeClass('duk-icon-close')
+          toggleMenu.addClass('duk-icon-bars');
+        }
+      });
+    }
+
+  };
+  
+})(jQuery);
