4432902f1dca8082202fd26c6516fea991a8634d
mikebernal <mikebernal.me@gmail.com>
2019-07-01 15:46:25 +0800

    Wrapped jQuery into the Drupal.behavior

--- web/themes/custom/omegasubtheme/js/app.js
+++ web/themes/custom/omegasubtheme/js/app.js
@@ -1,27 +1,25 @@
 (function ($) { 
-  // "use strict";
+  "use strict";
   Drupal.behaviors.toggleMenu = {         
     attach: function (context, settings) {
+      var toggleMenu = $('.duk-icon-bars');
+      var menuMain   = $('.menu--main');
       $('.duk-icon-bars', context).click(function () {
-
-        var toggleMenu = $('.duk-icon-bars', context);
-        var menuMain   = $('.menu--main', context);
 
         if ( (menuMain.css('display') === '') || (menuMain.css('display') === 'none') ) {
 
           menuMain.css('display', 'block');
-
           toggleMenu.removeClass('duk-icon-bars');
           toggleMenu.addClass('duk-icon-close');
-          console.log();
-
+          
         } else {
 
           menuMain.css('display', 'none');
-
           toggleMenu.removeClass('duk-icon-close')
           toggleMenu.addClass('duk-icon-bars');
+
         }
+
       });
     }
 
